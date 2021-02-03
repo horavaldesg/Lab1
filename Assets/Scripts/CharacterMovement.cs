2ca18e9350adf97ebed07a6c23b96c00011df00d
Horacio Valdes <horavaldesg@gmail.com>
2021-02-03 17:23:51 -0500

    Run

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private Animator animator;
+	[SerializeField] private float walkSpeed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private float facing = 1;
+
+	private void Update()
+	{
+		float input = (Input.GetAxisRaw("Horizontal"));
+
+		facing = input;
+
+		//if (facing < 0)
+		//{
+		//	transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0f, 270f, 0f), Time.deltaTime * 4f);
+		//}
+		//else if (facing > 0)
+		//{
+		//	transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0f, 90f, 0f), Time.deltaTime * 4f);
+		//}
+
+		transform.position += new Vector3(0f, 0f, input * walkSpeed * Time.deltaTime);
+
+		animator.SetFloat("Speed", Mathf.Abs(input));
+	}
 }
