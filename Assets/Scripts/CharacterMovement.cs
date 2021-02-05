8b0f8114759fc98991bc124407c34c9c0e47fd7b
Horacio Valdes <horavaldesg@gmail.com>
2021-02-05 17:07:49 -0500

    Movement Animation

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,14 +6,47 @@ public class CharacterMovement : MonoBehaviour
 {
 	[SerializeField] private Animator animator;
 	[SerializeField] private float walkSpeed;
-
+	private float speedboost = 0;
+	float t = 0;
 	private float facing = 1;
 
 	private void Update()
 	{
 		float input = (Input.GetAxisRaw("Horizontal"));
-
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D))
+        {
+			speedboost = 1.5f;
+			input = Mathf.Lerp(input, 1.5f, t);
+			t += 0.01f;
+			//input = 1.5f;
+			
+        }
+        
+		if(Input.GetKeyUp(KeyCode.LeftShift) && Input.GetKey(KeyCode.D))
+        {
+			t = 0;
+			
+		}
+		else if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.A))
+        {
+			speedboost = 1.5f;
+			input = Mathf.Lerp(input, -1.5f, -0.2f);
+			//input = -1.5f;
+		}
+        else
+        {
+			speedboost = 1;
+        }
 		facing = input;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+			animator.SetBool("Jump", true);
+        }
+		else if (Input.GetKeyUp(KeyCode.Space))
+		{
+			animator.SetBool("Jump", false);
+		}
 
 		//if (facing < 0)
 		//{
@@ -24,8 +57,9 @@ public class CharacterMovement : MonoBehaviour
 		//	transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0f, 90f, 0f), Time.deltaTime * 4f);
 		//}
 
-		transform.position += new Vector3(0f, 0f, input * walkSpeed * Time.deltaTime);
+		transform.position += new Vector3(0f, 0f, input * walkSpeed * speedboost * Time.deltaTime);
 
-		animator.SetFloat("Speed", Mathf.Abs(input));
+		animator.SetFloat("Speed", input);
+		Debug.Log(walkSpeed * speedboost);
 	}
 }
